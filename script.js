4d692d260d533d16495809a8059880c739e953ca
Arafat <arafatshaikh823@gmail.com>
2023-05-22 03:23:40 +0530

    styles added

---
+++ script.js
@@ -0,0 +1,9 @@
+const optionBtn = document.querySelectorAll(".option-button");
+const advanceBtn = documen.querySelectorAll(".adv-option-button");
+const scriptBtn = document.querySelectorAll(".script");
+const alignBtn = document.querySelectorAll(".align");
+const spacingBtn = document.querySelectorAll(".spacing");
+const format = document.querySelector(".format");
+const fontName = document.getElementById("fontName");
+const fontSize = document.getElementById("fontSize");
+
